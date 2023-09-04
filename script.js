ea11b2019c17502cac7bb24ba81edaf67c730366
hei0 <87489832+xec222@users.noreply.github.com>
2023-09-04 20:36:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const text = `
+     Hi, I'm doas, programmer / red teaming solving ctf's and more
+     i like C, Python,
+     I'm Front-End Developer
+     Arch Linux enjoyer
+     I have xmpp / matrix
+     Feel Free To contact with me
+
+     my email : doasman@proton.me
+     
+     
+     
+
+`;
+
+
+
+function openProjectTerminal() {
+    const projectTerminal = document.querySelector(".project-terminal");
+    projectTerminal.style.display = "block";
+}
+
+function closeProjectTerminal() {
+    const projectTerminal = document.querySelector(".project-terminal");
+    projectTerminal.style.display = "none";
+}
+
+
+
+const typedText = document.getElementById("typed-text");
+
+function typeText() {
+    let index = 0;
+    typedText.innerHTML = '';
+    const interval = setInterval(function () {
+        typedText.innerHTML += text[index];
+        index++;
+        if (index === text.length) {
+            clearInterval(interval);
+        }
+    }, 50);
+}
+
+window.onload = typeText;
+
+function closeTerminal() {
+    
+}
+
+function minimizeTerminal() {
+    // Terminal penceresini simge durumuna küçültme işlemi burada yapılabilir.
+}
+
+function maximizeTerminal() {
+    // Terminal penceresini tam ekran yapma işlemi burada yapılabilir.
+}
+
+function openEmailDialog() {
+    const modal = document.getElementById("email-modal");
+    modal.style.display = "block";
+}
+
+function closeEmailDialog() {
+    const modal = document.getElementById("email-modal");
+    modal.style.display = "none";
+}
+
+const emailForm = document.getElementById("email-form");
+emailForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const to = document.getElementById("doasman@proton.me").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    // E-posta gönderme işlemi burada yapılabilir.
+    // Örneğin, JavaScript kullanarak bir e-posta API'si ile iletişim kurabilirsiniz.
+    // Bu API, gerçek bir e-posta gönderme işlemi gerçekleştirecektir.
+    // Örnek API'yi kullanmak için ilgili belgeleri okuyun ve uygun şekilde yapılandırın.
+});
